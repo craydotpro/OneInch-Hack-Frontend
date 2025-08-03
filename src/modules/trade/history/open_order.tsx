8cbf038743400637a93c4de6de4b781218dd9d22
ptsayli <ptsayli@gmail.com>
2025-08-03 16:41:02 +0400

    fix: open order list

--- src/modules/trade/history/open_order.tsx
+++ src/modules/trade/history/open_order.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import insightService from "../../../services/insight";
 import { useAccount } from "wagmi";
+import insightService from "../../../services/insight";
 
 const OpenOrders = () => {
   const { address } = useAccount();
@@ -11,18 +11,18 @@ const OpenOrders = () => {
   });
   return (
     <>
-      <div className="grid grid-cols-4 font-bold text-xs py-2 text-center text-gray-500 sticky top-0 bg-white">
+      <div className="grid grid-cols-3 font-bold text-xs py-2 text-center text-gray-500 sticky top-0 bg-white">
         <div>Type</div>
-        <div>Token</div>
-        <div>Price</div>
-        <div>Amount</div>
+        {/* <div>Token</div> */}
+        <div>You Pay</div>
+        <div>You Receive</div>
       </div>
       {openOrders.data?.map((data: any) => (
-        <div className="grid grid-cols-4 font-bold text-xs py-2 text-center text-gray-500">
+        <div className="grid grid-cols-3 font-bold text-xs py-2 text-center text-gray-500">
           <div>{data.OrderType}</div>
-          <div>{data.token || "-"}</div>
-          <div>{data.price}</div>
-          <div>{data.amount}</div>
+          {/* <div>{data.token || "-"}</div> */}
+          <div>{data.youPay}</div>
+          <div>{data.youReceive}</div>
         </div>
       ))}
     </>
